d74f84e53da051f84a34740e2cf6e20924cf6143
Joshua Grose <jgrose@gmail.com>
2021-12-15 23:05:57 -0500

    test funtion

---
+++ functions/test.js
@@ -0,0 +1,16 @@
+// attaching function to exports
+
+exports.handler = async () => {
+    // log to console when this function runs so we can see where console logs are output
+    console.log('function ran')
+
+    // creat a constant and set it equal to an object with three properties, name age and job
+    const data = { name: 'josh', age: 38, job: 'be awesome'}
+
+    // return response to browser
+    return {
+        statusCode: 200,
+        body: JSON.stringify(data)
+    }
+
+}
